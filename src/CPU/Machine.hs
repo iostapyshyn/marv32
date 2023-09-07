f2f6a01a9abcaa0ced21a660bb524e2e534d5a4c
Illia Ostapyshyn <ostapyshyn@sra.uni-hannover.de>
2023-09-07 15:23:46 +0200

    Do not include rv32m extension for now

--- src/CPU/Machine.hs
+++ src/CPU/Machine.hs
@@ -36,24 +36,24 @@ data AluOp = AluAdd | AluSub | AluSll | AluSlt | AluSltu | AluAnd | AluOr
            | AluDiv | AluDivu | AluRem | AluRemu
   deriving (Show, Generic, NFDataX)
 
-mulH :: (Integral a, Integral b, Integral c) => a -> b -> c
-mulH a b = fromIntegral $ mulH64 (fromIntegral a) (fromIntegral b)
-  where mulH64 :: Unsigned 64 -> Unsigned 64 -> Unsigned 64
-        mulH64 a64 b64 = shiftR (a64 * b64) 32
+-- mulH :: (Integral a, Integral b, Integral c) => a -> b -> c
+-- mulH a b = fromIntegral $ mulH64 (fromIntegral a) (fromIntegral b)
+--   where mulH64 :: Unsigned 64 -> Unsigned 64 -> Unsigned 64
+--         mulH64 a64 b64 = shiftR (a64 * b64) 32
 
 runAlu :: AluOp -> Vec 2 AluOpand -> AluResult
 runAlu op (a :> b :> Nil) =
   case op of
     AluAdd  -> as + bs
     AluSub  -> as - bs
-    AluMul  -> as * bs
-    AluMulh   -> mulH as bs
-    AluMulhsu -> mulH as bu
-    AluMulhu  -> mulH au bu
-    AluDiv  -> as `div` bs
-    AluRem  -> as `rem` bs
-    AluDivu -> fromIntegral $ au `div` bu
-    AluRemu -> fromIntegral $ au `rem` bu
+    -- AluMul  -> as * bs
+    -- AluMulh   -> mulH as bs
+    -- AluMulhsu -> mulH as bu
+    -- AluMulhu  -> mulH au bu
+    -- AluDiv  -> as `div` bs
+    -- AluRem  -> as `rem` bs
+    -- AluDivu -> fromIntegral $ au `div` bu
+    -- AluRemu -> fromIntegral $ au `rem` bu
     AluSll  -> shiftL as bi
     AluSrl  -> fromIntegral $
                shiftR au bi
@@ -63,6 +63,7 @@ runAlu op (a :> b :> Nil) =
     AluAnd  -> as .&. bs
     AluXor  -> xor as bs
     AluOr   -> as .|. bs
+    _       -> error "Unsupported ALU operation"
   where as = fromIntegral a :: MWordS -- signed
         au = fromIntegral a :: MWordU -- unsigned
         bs = fromIntegral b :: MWordS -- signed
